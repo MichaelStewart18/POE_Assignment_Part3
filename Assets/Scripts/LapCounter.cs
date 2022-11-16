f3c7f1b4d1982c9b400cae4170ce191106defc7b
Michael Stewart <michaelandrewstew@gmail.com>
2022-11-16 13:12:51 +0200

    Fixed lap Counter

--- Assets/Scripts/LapCounter.cs
+++ Assets/Scripts/LapCounter.cs
@@ -8,19 +8,22 @@ public class LapCounter : MonoBehaviour
     public GameObject finishLap;
     public Text lapCounter;
     public SFXManager sfxManager;
+    public int lapCount = 0;
 
     private void OnTriggerEnter(Collider other)
     {
-        int lapCount = 1;
-
+        
+        lapCount++;
         if(other.gameObject.CompareTag("StartLine"))
         {
             if (lapCount < 3)
             {
                 lapCounter.text = "Lap: " + lapCount;
-                lapCount++;
+                
 
+                Debug.Log("passed the checkpoint");
                 Debug.Log(lapCount);
+
                 //sfxManager.PlaySound("Win");
             } 
             else if(lapCount >= 3)
